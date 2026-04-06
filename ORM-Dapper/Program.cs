224d93cd35963d25a5bfc8a6a03754a19ebd4512
bryantwhitsett <bryantwhitsett71@gmail.com>
2026-04-06 11:15:52 -0400

    initial commit

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace ORM_Dapper
+﻿using System.Data;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection")!;
+
+            using IDbConnection conn = new MySqlConnection(connString);
+
+            Console.WriteLine("Connection object created successfully!"); //OmniSharp: Restart OmniSharpX
         }
     }
 }
